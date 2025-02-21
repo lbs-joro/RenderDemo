91e980409eb5386d7aa7e15eef014a8a13de8ee6
lbs-joro <johan.ronnas@ga.lbs.se>
2025-02-21 13:58:28 +0100

    La till databasuppkoppling

--- test.js
+++ test.js
@@ -1,9 +1,27 @@
 const express = require("express");
+const mysql = require("mysql2");
+const fs = require("fs");
+
+const connection = mysql.createConnection({
+    host:process.env.DB_HOST,
+    port:process.env.DB_PORT,
+    user:'avnadmin',
+    password:process.env.DB_PASS,
+    database:'defaultdb',
+    ssl: {ca:fs.readFileSync(__dirname + "/mysql-ca.crt")}
+})
 
 const app = express();
 
 app.get("/", function(req, res){
-    res.send("Hej från Johan!");
+    connection.query("SELECT * FROM testtable", function(error, result){
+        if(error){
+            res.send("Något gick fel!");
+        }else{
+            res.send(result[0]);
+        }
+    })
+    
 })
 
 app.listen(3000);
